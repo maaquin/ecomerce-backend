57a4edd88e8d2c56c8bdf3ebc8bc3a403cbbc9f8
maaquin <lucianomaquin@gmail.com>
2025-08-18 20:44:44 -0600

    origin

--- api/index.js
+++
@@ -1,8 +0,0 @@
-import Server from "./configs/server.js";
-
-const server = new Server();
-
-// Exporta la app como handler para Vercel
-export default function handler(req, res) {
-    server.app(req, res);
-}
