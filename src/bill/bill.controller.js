be6cb691a16fd1ff390142a760598f12f8072cba
maaquin <lucianomaquin@gmail.com>
2025-08-20 11:26:53 -0600

    4

--- src/bill/bill.controller.js
+++ src/bill/bill.controller.js
@@ -164,12 +164,11 @@ export const getAllBills = async (req, res) => {
  * @route   GET /api/bill/:billCode
  */
 export const getBillById = async (req, res) => {
-    const { billCode } = req.params;
-    console.log(`RECIBIDO billCode: |${billCode}|`);
+    const { id } = req.params;
     const sqlQuery = 'SELECT * FROM Bill WHERE billCode = ?';
     try {
         // 1. Ejecutamos la consulta
-        const [billRows] = await pool.query(sqlQuery, [billCode]);
+        const [billRows] = await pool.query(sqlQuery, [id]);
 
         // 2. Verificamos si el array de resultados tiene algo
         if (billRows) {
